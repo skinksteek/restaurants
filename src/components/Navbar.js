2f3931fcb3d719e74e5edb38da3c8d81d296b73b
Linus <linusigelstrom@gmail.com>
2024-10-14 20:27:15 +0200

    progress with the navbar, simple styling and font

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,41 @@
 import { Link } from "react-router-dom";
+import "../assets/styles/navbar.css";
+import logo from "../assets/images/logo.png";
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Hem</Link>
-        </li>
-        <li>
-          <Link to="/meny">Meny</Link>
-        </li>
-        <li>
-          <Link to="/kontakt">Kontakt</Link>
-        </li>
-        <li>
-          <Link to="/hittahit">Hitta hit</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="nav-container">
+      <nav className="nav-wrapper">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/" className="hover-underline">
+              Hem
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/meny" className="hover-underline">
+              Meny
+            </Link>
+          </li>
+          <div className="logo-wrapper">
+            <Link to="/">
+              <a href="/">
+                <img src={logo} alt="Restaurang" title="Gå till startsidan" />
+              </a>
+            </Link>
+          </div>
+          <li className="nav-item">
+            <Link to="/kontakt" className="hover-underline">
+              Kontakt
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/hittahit" className="hover-underline">
+              Hitta hit
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
