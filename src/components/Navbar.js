7ea85952633b4f20b5dfcd3912f70434518372ab
Linus <linusigelstrom@gmail.com>
2024-10-29 17:21:53 +0100

    code cleanup, styling changes and added better looking hover for the nav, pausing work on nav for now until i can figure out how i want the color combinations

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,38 +4,36 @@ import logo from "../assets/images/logo-trans.png";
 
 export default function Navbar() {
   return (
-    <div className="nav-container">
-      <nav className="nav-wrapper">
-        <ul className="nav-list">
-          <li className="nav-item">
-            <Link to="/" className="hover-underline">
-              Hem
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/meny" className="hover-underline">
-              Meny
-            </Link>
-          </li>
-          <div className="logo-wrapper">
-            <Link to="/">
-              <a href="/">
-                <img src={logo} alt="Restaurang" title="Gå till startsidan" />
-              </a>
-            </Link>
-          </div>
-          <li className="nav-item">
-            <Link to="/kontakt" className="hover-underline">
-              Kontakt
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/hittahit" className="hover-underline">
-              Hitta hit
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="nav-wrapper">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link to="/" className="hover-underline">
+            Hem
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/meny" className="hover-underline">
+            Meny
+          </Link>
+        </li>
+        <div className="logo-wrapper">
+          <Link to="/">
+            <a href="/">
+              <img src={logo} alt="Restaurang" title="Gå till startsidan" />
+            </a>
+          </Link>
+        </div>
+        <li className="nav-item">
+          <Link to="/kontakt" className="hover-underline">
+            Kontakt
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/hittahit" className="hover-underline">
+            Hitta hit
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
