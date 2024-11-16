07cd1081481aed923b65670e67cb4300f0e8bf55
Linus <linusigelstrom@gmail.com>
2024-11-16 23:44:46 +0100

    styling changes to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -10,10 +10,6 @@ export default function Button({ text, href, onClick }) {
   return (
     <a href={href} className="button" onClick={handleClick}>
       <span>{text}</span>
-      <svg width="13px" height="10px" viewBox="0 0 13 10">
-        <path d="M1,5 L11,5"></path>
-        <polyline points="8 1 12 5 8 9"></polyline>
-      </svg>
     </a>
   );
 
