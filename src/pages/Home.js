d99789028854eb7a284177b5b60e31199723440f
Linus <linusigelstrom@gmail.com>
2024-11-17 12:00:44 +0100

    started to work on the content of homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,16 @@ export default function Home() {
   return (
     <>
       <Hero title="Baking real pizza since 1337" />
-      <section className="home-page-container main-content">
-        <p>Hem</p>
+      <section className="homepage-container main-content">
         <Menu />
+        <div class="homepage-grid-wrapper">
+          <div class="grid-1">1</div>
+          <div class="grid-2">2</div>
+          <div class="grid-3">3</div>
+          <div class="grid-4">4</div>
+          <div class="grid-5">5</div>
+          <div class="grid-6">6</div>
+        </div>
       </section>
     </>
   );
