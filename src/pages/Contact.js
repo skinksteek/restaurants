83b9f51d48d859c274dc35a9ff16efe26cbee79a
Linus <linusigelstrom@gmail.com>
2024-12-05 21:02:01 +0100

    added hero image for contact page, and also added content for contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,32 @@
+import restaurantcontact from "../assets/images/restaurant-contact.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import Hero from "../components/Hero";
+
 export default function Contact() {
   return (
-    <div className="main-content">
-      <p>Kontakt</p>
-    </div>
+    <section className="contactpage-container main-content">
+      <Hero
+        title="Kontakta oss"
+        bread="INGA BORDSBOKNIGNAR – ENBART DROP IN."
+        info="TAKE AWAY ETT SAMTAL BORT"
+        contact={
+          <span>
+            <a
+              id="phone-hover"
+              href="tel:+0046702785881"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faPhone} size="2xl" />
+            </a>
+            +46 70 27 85 881
+          </span>
+        }
+        mediaType="image"
+        mediaSrc={restaurantcontact}
+        altText="Foto från insidan av restaurangen"
+      ></Hero>
+    </section>
   );
 }
